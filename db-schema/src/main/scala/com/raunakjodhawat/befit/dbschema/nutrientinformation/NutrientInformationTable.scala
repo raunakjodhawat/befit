ce31edfc6a8d87ff5f527393184079129fe83f1f
Raunak Jodhawat <raunakjodhawat@gmail.com>
2024-02-13 17:08:33 +0530

    Change name

--- db-schema/src/main/scala/com/raunakjodhawat/befit/dbschema/nutrientinformation/NutrientInformationTable.scala
+++ db-schema/src/main/scala/com/raunakjodhawat/befit/dbschema/nutrientinformation/NutrientInformationTable.scala
@@ -6,7 +6,7 @@ import slick.lifted.{ProvenShape, Rep, Tag}
 class NutrientInformationTable(tag: Tag)
     extends Table[NutrientInformation](tag, "NUTRIENTINFORMATION") {
 
-  def name: Rep[String] = column[String]("NAME")
+  def name: Rep[String] = column[String]("S_NAME")
   def protein: Rep[Option[Double]] = column[Option[Double]]("PROTEIN")
   def carbohydrate: Rep[Option[Double]] = column[Option[Double]]("CARBOHYDRATE")
   def fat: Rep[Option[Double]] = column[Option[Double]]("FAT")
